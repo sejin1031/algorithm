3874b75165c8727cc14afdbf7be21fdc6fdce82d
sejin.eom <sejin1031@naver.com>
2020-01-03 23:19:31 +0900

    test

---
+++ src/Test2.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Test2 {
+    public static  void  main(String... args){
+        Scanner scn = new Scanner(System.in);
+        String name = scn.next();
+        System.out.println(name);
+    }
+}
